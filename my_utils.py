8cd862e243ae43f6f91795b16834e1a92b515380
serenashen484 <131408705+serenashen484@users.noreply.github.com>
2023-11-25 02:22:33 -0500

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -4,9 +4,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import math
-import torch
-from torch.utils.data import Dataset
 
 # reorganzie columns
 def reorganize_cols(df):
@@ -102,29 +99,6 @@ def plot_population(y_true, y_pred, title='Results', flag='plot', anomaly_dateti
     fig.show()
     
     return fig
-
-class SequenceDataset(Dataset):
-    def __init__(self, dataframe, seq_len_src=56, seq_len_tgt=8, lag_len=1):
-        self.seq_len_src = seq_len_src
-        self.lag_len = lag_len
-        self.seq_len_tgt = seq_len_tgt
-        self.df = torch.tensor(dataframe.values).float()
-        self.len = math.floor((len(self.df) - self.seq_len_src - self.seq_len_tgt) / self.lag_len) + 1
-
-    def __len__(self):
-        return self.len
-
-    def __getitem__(self, i):
-        if type(i) == slice:
-            raise Exception('Exception: Expected int, inputted slice!')
-
-        X = self.df[i*self.lag_len:i*self.lag_len+self.seq_len_src]
-        Y = self.df[i*self.lag_len+self.seq_len_src:i*self.lag_len+self.seq_len_src+self.seq_len_tgt]
-
-        return X, Y
-
-    def input_size(self):
-        return self.df.shape[1]
 
 # my ss class used for standardize data
 class MyStandardScaler:
