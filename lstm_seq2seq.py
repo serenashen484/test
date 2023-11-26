3c3360f17532c8d01a04759f57c68484264437b3
serenashen484 <131408705+serenashen484@users.noreply.github.com>
2023-11-26 02:51:59 -0500

    Update lstm_seq2seq.py

--- lstm_seq2seq.py
+++ lstm_seq2seq.py
@@ -118,7 +118,7 @@ class LSTM_seq2seq(nn.Module):
 
 def lstm_train(model, data_loader):
     total_loss = []
-    for n_batches, (src, tgt) in enumerate(data_loader):
+    for _, (src, tgt) in enumerate(data_loader):
         src = src.to(model.device)
         tgt = tgt.to(model.device)
 
